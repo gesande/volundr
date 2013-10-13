64a077ee3a7c579d51b721ede130f6e4c6c98a2e
Geir Sande <geir@geir-ubuntu.(none)>
2013-10-13 13:41:04 +0300

    added visiting gzip stream to smithy

--- völundr-smithy/src/main/java/net/sf/völundr/VölundrSmithy.java
+++ völundr-smithy/src/main/java/net/sf/völundr/VölundrSmithy.java
@@ -5,6 +5,7 @@ import java.nio.charset.Charset;
 
 import net.sf.völundr.bag.StronglyTypedSortedBag;
 import net.sf.völundr.io.AsynchronousStreamReader;
+import net.sf.völundr.io.GZipStreamReaderFactory;
 import net.sf.völundr.io.InputStreamReaderFactory;
 import net.sf.völundr.io.InputStreamToLines;
 import net.sf.völundr.io.InputStreamToString;
@@ -52,6 +53,13 @@ public final class VölundrSmithy {
 			final VisitingInputStreamsHandler handler,
 			final StreamReadFailedNotifier readFailedNotifier) {
 		return new VisitingInputStreams(handler, new InputStreamReaderFactory(
+				charset()), readFailedNotifier);
+	}
+
+	public VisitingInputStreams visitingGzipInputStreams(
+			final VisitingInputStreamsHandler handler,
+			final StreamReadFailedNotifier readFailedNotifier) {
+		return new VisitingInputStreams(handler, new GZipStreamReaderFactory(
 				charset()), readFailedNotifier);
 	}
 
