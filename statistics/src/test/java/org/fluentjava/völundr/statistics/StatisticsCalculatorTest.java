33ac8f237597af38949b99ca83fc80a929df93d5
Geir Sande <geir.sande@luukku.com>
2018-03-08 15:59:54 +0200

    more unit tests for medianresolver (long values odd/even)

--- statistics/src/test/java/org/fluentjava/völundr/statistics/StatisticsCalculatorTest.java
+++ statistics/src/test/java/org/fluentjava/völundr/statistics/StatisticsCalculatorTest.java
@@ -74,6 +74,70 @@ public class StatisticsCalculatorTest {
     }
 
     @Test
+    public void statsEvenLongValues() {
+        final List<Long> values = new ArrayList<>();
+        for (long i = 100; i > 0; i--) {
+            values.add(i);
+        }
+        StatisticsListProvider<Long> stat = StatisticsListProviderFactory
+                .longValues(values);
+        assertEquals("Min doesn't match!", 1, stat.min(), 0);
+        assertEquals("Max doesn't match!", 100, stat.max(), 0);
+        assertEquals("Mean doesn't match!", 50.5, stat.mean(), 0);
+        assertEquals("Median doesn't match!", 50, stat.median(), 0);
+        assertEquals("50 percentile doesn't match!", 51, stat.percentile(50),
+                0);
+        assertEquals("90 percentile doesn't match!", 91, stat.percentile(90),
+                0);
+        assertEquals("95 percentile doesn't match!", 96, stat.percentile(95),
+                0);
+        assertEquals("96 percentile doesn't match!", 97, stat.percentile(96),
+                0);
+        assertEquals("97 percentile doesn't match!", 98, stat.percentile(97),
+                0);
+        assertEquals("98 percentile doesn't match!", 99, stat.percentile(98),
+                0);
+        assertEquals("99 percentile doesn't match!", 100, stat.percentile(99),
+                0);
+        assertEquals("Standard deviation doesn't match!", 28.86607004772212,
+                stat.standardDeviation(), 0);
+        assertEquals("Standard deviation doesn't match!", 833.25,
+                stat.variance(), 0);
+    }
+
+    @Test
+    public void statsOddLongValues() {
+        final List<Long> values = new ArrayList<>();
+        for (long i = 100; i > -1; i--) {
+            values.add(i);
+        }
+        StatisticsListProvider<Long> stat = StatisticsListProviderFactory
+                .longValues(values);
+        assertEquals("Min doesn't match!", 0, stat.min(), 0);
+        assertEquals("Max doesn't match!", 100, stat.max(), 0);
+        assertEquals("Mean doesn't match!", 50, stat.mean(), 0);
+        assertEquals("Median doesn't match!", 50, stat.median(), 0);
+        assertEquals("50 percentile doesn't match!", 50, stat.percentile(50),
+                0);
+        assertEquals("90 percentile doesn't match!", 90, stat.percentile(90),
+                0);
+        assertEquals("95 percentile doesn't match!", 95, stat.percentile(95),
+                0);
+        assertEquals("96 percentile doesn't match!", 96, stat.percentile(96),
+                0);
+        assertEquals("97 percentile doesn't match!", 97, stat.percentile(97),
+                0);
+        assertEquals("98 percentile doesn't match!", 98, stat.percentile(98),
+                0);
+        assertEquals("99 percentile doesn't match!", 99, stat.percentile(99),
+                0);
+        assertEquals("Standard deviation doesn't match!", 29.154759474226502,
+                stat.standardDeviation(), 0);
+        assertEquals("Standard deviation doesn't match!", 850.0,
+                stat.variance(), 0);
+    }
+
+    @Test
     public void mean() {
         StatisticsCalculator stat = StatisticsCalculator
                 .fromValues(sampleList());
