1b677482266d4d77bb127d0586936c1ca12c0b39
Geir Sande <geir.sande@luukku.com>
2018-03-21 14:02:48 +0200

    moving graph stuff from perftence

---
+++ graph-jfreechart/src/main/java/org/fluentjava/völundr/graph/GraphOptions.java
@@ -0,0 +1,15 @@
+package org.fluentjava.völundr.graph;
+
+public interface GraphOptions {
+
+    boolean provideStatistics();
+
+    String xAxisTitle();
+
+    String title();
+
+    String legendTitle();
+
+    int range();
+
+}
