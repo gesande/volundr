e18a9ac5baf7fad541d1e4df8da4b2b3c01b2116
Geir Sande <geir.sande@luukku.com>
2021-09-22 13:16:10 +0300

    nebula.lint works (magically)

--- licenseToCommit.sh
+++ licenseToCommit.sh
@@ -3,6 +3,8 @@ set -eu
 ./gradlew spotlessCheck
 ./gradlew -Dorg.gradle.parallel=true build test --info
 ./gradlew -Dorg.gradle.parallel=false aggregateTestReport jacocoTestReport jacocoAggregateReport
+./gradlew -Dorg.gradle.parallel=false criticalLintGradle
 ./gradlew -Dorg.gradle.parallel=false --info dependencyUpdates -DoutputFormatter=html
 ./gradlew -Dorg.gradle.parallel=false --info dependencyCheckAggregate -Dcom.sun.security.enableAIAcaIssuers=true
+./gradlew -Dorg.gradle.parallel=false generateGradleLintReport
 ./gradlew -Dorg.gradle.parallel=false archiveAggregateReports
