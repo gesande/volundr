4b799ee35bc47c2ceb142cafd5a271ca57002138
Geir Sande <geir.sande@luukku.com>
2022-01-03 12:00:15 +0200

    update golden masters, using Monospaced font for graphs

--- graph-jfreechart/src/main/java/org/fluentjava/volundr/graph/jfreechart/JFreeChartFactory.java
+++ graph-jfreechart/src/main/java/org/fluentjava/volundr/graph/jfreechart/JFreeChartFactory.java
@@ -1,6 +1,7 @@
 package org.fluentjava.volundr.graph.jfreechart;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import org.fluentjava.volundr.graph.ImageData;
 import org.jfree.chart.ChartFactory;
@@ -13,12 +14,35 @@ final class JFreeChartFactory {
     private static final Logger LOGGER = LoggerFactory
             .getLogger(ImageFactoryUsingJFreeChart.class);
 
+    private static final String FONT_FAMILY_NAME = "Monospaced";
+    private static final Font TITLE_FONT = new Font(FONT_FAMILY_NAME, Font.BOLD,
+            20);
+    private static final Font LEGEND_FONT = new Font(FONT_FAMILY_NAME,
+            Font.PLAIN, 12);
+    private static final Font LABEL_FONT = new Font(FONT_FAMILY_NAME,
+            Font.PLAIN, 12);
+    private static final Font DOMAIN_AXIS_TICK_LABEL_FONT = new Font(
+            FONT_FAMILY_NAME, Font.PLAIN, 12);
+    private static final Font RANGE_AXIS_TICK_LABEL_FONT = new Font(
+            FONT_FAMILY_NAME, Font.PLAIN, 12);
+
     @SuppressWarnings("static-method")
     public JFreeChart newBarChart(final ImageData imageData) {
         LOGGER.info("Create bar chart...");
-        return ChartFactory.createBarChart(imageData.title(),
+        JFreeChart barChart = ChartFactory.createBarChart(imageData.title(),
                 imageData.xAxisLabel(), null, null, PlotOrientation.VERTICAL,
                 showLegend(), noTooltips(), noUrls());
+        barChart.getTitle().setFont(TITLE_FONT);
+        barChart.getLegend().setItemFont(LEGEND_FONT);
+
+        barChart.getCategoryPlot().getDomainAxis()
+                .setTickLabelFont(DOMAIN_AXIS_TICK_LABEL_FONT);
+        barChart.getCategoryPlot().getDomainAxis().setLabelFont(LABEL_FONT);
+
+        barChart.getCategoryPlot().getRangeAxis()
+                .setTickLabelFont(RANGE_AXIS_TICK_LABEL_FONT);
+        barChart.getCategoryPlot().getRangeAxis().setLabelFont(LABEL_FONT);
+        return barChart;
     }
 
     @SuppressWarnings("static-method")
@@ -27,18 +51,41 @@ final class JFreeChartFactory {
         final XYSeriesAdapterForScatterPlot adapter = (XYSeriesAdapterForScatterPlot) imageData
                 .adapter();
         final ScatterPlotGraphData graphData = adapter.graphData(Color.RED, 0);
-        return ChartFactory.createScatterPlot(imageData.title(),
+        JFreeChart scatterPlot = ChartFactory.createScatterPlot(imageData.title(),
                 imageData.xAxisLabel(), graphData.yAxisTitle(),
                 graphData.dataset(), PlotOrientation.VERTICAL, showLegend(),
                 noTooltips(), noUrls());
+
+        scatterPlot.getTitle().setFont(TITLE_FONT);
+        scatterPlot.getLegend().setItemFont(LEGEND_FONT);
+
+        scatterPlot.getXYPlot().getDomainAxis().setLabelFont(LABEL_FONT);
+        scatterPlot.getXYPlot().getDomainAxis()
+                .setTickLabelFont(DOMAIN_AXIS_TICK_LABEL_FONT);
+
+        scatterPlot.getXYPlot().getRangeAxis()
+                .setTickLabelFont(RANGE_AXIS_TICK_LABEL_FONT);
+        scatterPlot.getXYPlot().getRangeAxis().setLabelFont(LABEL_FONT);
+        return scatterPlot;
     }
 
     @SuppressWarnings("static-method")
     public JFreeChart newXYLineChart(final ImageData imageData) {
         LOGGER.info("Create XY linechart...");
-        return ChartFactory.createXYLineChart(imageData.title(),
+        JFreeChart lineChart = ChartFactory.createXYLineChart(imageData.title(),
                 imageData.xAxisLabel(), null, null, PlotOrientation.VERTICAL,
                 showLegend(), noTooltips(), noUrls());
+        lineChart.getTitle().setFont(TITLE_FONT);
+        lineChart.getLegend().setItemFont(LEGEND_FONT);
+
+        lineChart.getXYPlot().getDomainAxis()
+                .setTickLabelFont(DOMAIN_AXIS_TICK_LABEL_FONT);
+        lineChart.getXYPlot().getDomainAxis().setLabelFont(LABEL_FONT);
+
+        lineChart.getXYPlot().getRangeAxis()
+                .setTickLabelFont(RANGE_AXIS_TICK_LABEL_FONT);
+        lineChart.getXYPlot().getRangeAxis().setLabelFont(LABEL_FONT);
+        return lineChart;
     }
 
     private static boolean showLegend() {
